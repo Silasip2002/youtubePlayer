9a6bd51b05ef89d30c37c9d9a2036997c0aa6e58
Silasip2002 <59552462+Silasip2002@users.noreply.github.com>
2025-04-09 22:36:35 -0400

    Delete services/config.js

--- services/config.js
+++
@@ -1,6 +0,0 @@
-// YouTube API configuration
-export const YOUTUBE_API_KEY = 'AIzaSyAOtR8Va5Lw6lU2f5ppUIIm2yCyZ2PPpZ8';
-export const YOUTUBE_API_BASE_URL = 'https://www.googleapis.com/youtube/v3';
-
-// Maximum results to fetch per request
-export const MAX_RESULTS = 10;
